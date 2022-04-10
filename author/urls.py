d99d96d69df0dadb92eda86447402d2edb6e1233
Anna Khishchenko <ann.kh18@gmail.com>
2022-04-10 17:14:13 +0300

    Added url for DRF

--- author/urls.py
+++ author/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('author', views.AuthorView)
 
 urlpatterns = [
-    path('', AuthorList.as_view(), name='author'),
-    path('form/', author_form, name='author_insert'),
-    path('form/<int:id>', author_form, name='author_update'),
-   
+    path('', views.AuthorList.as_view(), name='author'),
+    path('form/', views.author_form, name='author_insert'),
+    path('form/<int:id>', views.author_form, name='author_update'),
+    path('', include(router.urls)),
 ]
