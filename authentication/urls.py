98394c8ba2ed494249776f03158569ab0282336d
Anna Khishchenko <ann.kh18@gmail.com>
2022-04-10 16:20:14 +0300

    Added url for DRF

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from .views import *
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('user', views.CustomUserView)
 
 urlpatterns = [
-    path('', user_form, name='user_form'),
-    path('<int:id>/', user_form, name='edit_user'),
-    path('all/', UserList.as_view(), name='users'),
-    path('overdue/', overdue, name='overdue')
+    path('form', views.user_form, name='user_form'),
+    path('<int:id>/', views.user_form, name='edit_user'),
+    path('all/', views.UserList.as_view(), name='users'),
+    path('overdue/', views.overdue, name='overdue'),
+    path('', include(router.urls))
 ]
